428677368ddd1b1a86d5469a04fadf74b9c9b050
vicente Alarcón <elcoeter@msn.com>
2022-01-17 16:13:20 +0100

    version final con javadoc DNI 5325278E

--- VicenteAG_53205278E.java
+++ VicenteAG_53205278E.java
@@ -3,17 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-
+/**
+ * EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+ * @author Vicente Alarcón González 53205278E
+ * @version 1.0
+ */
 public class VicenteAG_53205278E {
     static final double FACTOR_HORA_EXTRA = 1.2;
 private String nombreCompleto, dni;
 private double salarioBasePorHora = 10.56;
 
+/**
+ * Constructor con tres parámetros
+ * @param nombreCompleto nombre completo en mayúsculas
+ * @param dni documento nacional de identidad con letra y sin espacios.
+ */
 public VicenteAG_53205278E(String nombreCompleto, String dni) {
 this.nombreCompleto = nombreCompleto;
 this.dni = dni;
 }
-
+/**
+ * calcula el salario bruto mesual en función del salario base por hora y las horas extras.
+ * El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA.
+ * @param horasExtra número de horas extra dedicadas redondeado a la baja
+ * @return salarioFinal devuelve el salario bruto mensual en euros con dos decimales.
+ */
 public double getSalarioEsteMes(int horasExtra) {
 double cantidadExtra, salarioFinal;
 cantidadExtra = horasExtra * this.salarioBasePorHora *
